27bae29a2f15565e683bd3bb9c08db30c371cd15
Ayushman <singhayushman291@gmail.com>
2025-07-04 13:32:03 +0530

    New list of 110 stations

---
+++ stations.js
@@ -0,0 +1,732 @@
+array: [
+  {
+    name: "'Y' Junction Udhana Magdalla Road BRTS",
+    lat: 21.146092,
+    lng: 72.760957,
+    details:
+      "'Y' Junction Udhana Magdalla Road BRTS, Vesu, Rundh, Surat, Gujarat 394518, India",
+  },
+  {
+    name: "'Y' Junction Dumas Road BRTS",
+    lat: 21.144231,
+    lng: 72.757999,
+    details: "'Y' Junction Dumas Road BRTS, Vesu, Surat, Gujarat 394518, India",
+  },
+  {
+    name: "Maharana Pratap Junction BRTS",
+    lat: 21.148379,
+    lng: 72.766442,
+    details:
+      "Maharana Pratap Junction BRTS, Vesu, Rundh, Surat, Gujarat 394518, India",
+  },
+  {
+    name: "Magdalla T Junction BRTS",
+    lat: 21.147673,
+    lng: 72.760584,
+    details:
+      "Magdalla T Junction BRTS, Piplod, Rundh, Surat, Gujarat 394518, India",
+  },
+  {
+    name: "Jolly Residency",
+    lat: 21.146242,
+    lng: 72.768395,
+    details: "Jolly Residency, Vesu, Rundh, Surat, Gujarat 394518, India",
+  },
+  {
+    name: "S.D Planets School",
+    lat: 21.152731,
+    lng: 72.771855,
+    details: "S.D Planets School, Piplod, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "S.D. Jain School BRTS",
+    lat: 21.151883,
+    lng: 72.772,
+    details: "S.D. Jain School BRTS, Piplod, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "J.H. Ambani School BRTS",
+    lat: 21.152066,
+    lng: 72.775294,
+    details: "J.H. Ambani School BRTS, Vesu, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "Someshwar Junction BRTS",
+    lat: 21.153747,
+    lng: 72.779741,
+    details: "Someshwar Junction BRTS, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "Piplod Main Road",
+    lat: 21.154327,
+    lng: 72.779231,
+    details:
+      "Piplod Main Road, Sarunagar, Piplod, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "K.P Commerce College",
+    lat: 21.183411,
+    lng: 72.808864,
+    details: "K.P Commerce College, Athwa Gate, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Police Commissioner Office",
+    lat: 21.182311,
+    lng: 72.803757,
+    details:
+      "Police Commissioner Office, Chopati, Police Line, Athwa, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "City Civic Centre",
+    lat: 21.18039,
+    lng: 72.801418,
+    details:
+      "City Civic Centre, Navo Mohollow, Meghdoot Society, Athwalines, Athwa, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "Gandhi College",
+    lat: 21.181611,
+    lng: 72.815645,
+    details: "Gandhi College, Majura Gate, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Mahavir Hospital",
+    lat: 21.184932,
+    lng: 72.814035,
+    details:
+      "Mahavir Hospital, Opp. Gandhi Smruti Bhavan, Nanpura, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Girls Polytechnic",
+    lat: 21.186793,
+    lng: 72.810452,
+    details:
+      "Girls Polytechnic, Kharwawad, Nanpura, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Athvagate",
+    lat: 21.185972,
+    lng: 72.810066,
+    details: "Athvagate, Athwa Gate, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Athwagate",
+    lat: 21.185152,
+    lng: 72.811332,
+    details:
+      "Athwagate, Near Kalyan Apartment, Athwa Gate, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Man Mandir Apartment",
+    lat: 21.187713,
+    lng: 72.814851,
+    details:
+      "Man Mandir Apartment, Timaliawad, Nanpura, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Nanpura Post Office",
+    lat: 21.190254,
+    lng: 72.812963,
+    details: "Nanpura Post Office, Nanpura, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Bahumali",
+    lat: 21.188453,
+    lng: 72.810924,
+    details: "Bahumali, Kharwawad, Nanpura, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Kuvawadi",
+    lat: 21.189654,
+    lng: 72.817082,
+    details: "Kuvawadi, Timaliawad, Nanpura, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Machhiwad",
+    lat: 21.191814,
+    lng: 72.817168,
+    details: "Machhiwad, Machhiwar, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "BAPS Swaminarayan Sanstha",
+    lat: 21.192635,
+    lng: 72.807362,
+    details:
+      "BAPS Swaminarayan Sanstha, Anand Vatika Society, Bapu Nagar, Adajan, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Corporation School",
+    lat: 21.195275,
+    lng: 72.809615,
+    details:
+      "Corporation School, Bapu Nagar, Adajan, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Gujrat Gas Circle",
+    lat: 21.193515,
+    lng: 72.801483,
+    details:
+      "Gujrat Gas Circle, Vishal Nagar Society, Adajan Gam, Adajan, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Shakti Niketan Mandir",
+    lat: 21.196076,
+    lng: 72.807105,
+    details:
+      "Shakti Niketan Mandir, Jai Ambe Society, Adajan, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Spipa (Surat)",
+    lat: 21.194635,
+    lng: 72.793629,
+    details:
+      "Spipa (Surat), Shri Nath Society ,Adajan Gam, Adajan Gam, Adajan, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Shahi Masjid Adajan Gam",
+    lat: 21.191174,
+    lng: 72.794209,
+    details:
+      "Shahi Masjid Adajan Gam, Adajan Gam, Adajan, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Van Bhavan",
+    lat: 21.195155,
+    lng: 72.793168,
+    details:
+      "Van Bhavan, Shri Nath Society ,Adajan Gam, Adajan Gam, Adajan, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Shanti Kunj",
+    lat: 21.194795,
+    lng: 72.790486,
+    details:
+      "Shanti Kunj, Hariom Nagar Society,Adajan Gam, Adajan Gam, Adajan, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Green Avenue",
+    lat: 21.194375,
+    lng: 72.786183,
+    details:
+      "Green Avenue, TGB, Adajan Gam, Adajan, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Pal Gaam",
+    lat: 21.193525,
+    lng: 72.779896,
+    details: "Pal Gaam, Adajan Gam, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Trinity Business Park",
+    lat: 21.190874,
+    lng: 72.788683,
+    details:
+      "Trinity Business Park, Adajan Gam, Adajan, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "SMC Party Plot Pal",
+    lat: 21.190244,
+    lng: 72.782149,
+    details: "SMC Party Plot Pal, Adajan Gam, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Saurabh Society",
+    lat: 21.190474,
+    lng: 72.785422,
+    details:
+      "Saurabh Society, Adajan Gam, Adajan, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Sargam Shopping Centre",
+    lat: 21.171506,
+    lng: 72.790722,
+    details:
+      "Sargam Shopping Centre, City Light Town, Athwa, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "Shrungar Society City Light Road",
+    lat: 21.172507,
+    lng: 72.793554,
+    details:
+      "Shrungar Society City Light Road, Shringar Society, City Light Town, Athwa, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "Sushrut Hospital",
+    lat: 21.16948,
+    lng: 72.787578,
+    details:
+      "Sushrut Hospital, SVNIT Campus, Athwa, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "Umra Food Park",
+    lat: 21.168665,
+    lng: 72.784445,
+    details:
+      "Umra Food Park, Sri Nehru Nagar Society, Umra Gam, Athwa, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "Umra Gaam",
+    lat: 21.171606,
+    lng: 72.782144,
+    details:
+      "Umra Gaam, Raghuvir Society, Sri Nehru Nagar Society, Umra Gam, Athwa, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "Umra Health Centre",
+    lat: 21.171954,
+    lng: 72.779687,
+    details:
+      "Umra Health Centre, Umra Gam, Athwa, Rundh, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "Parle Point",
+    lat: 21.175053,
+    lng: 72.794619,
+    details: "Parle Point, Umra Gam, Athwa, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "V.T Choksi College",
+    lat: 21.17657,
+    lng: 72.796679,
+    details:
+      "V.T Choksi College, Meghdoot Society, Athwalines, Athwa, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "Gujarati School Ghod DoD Road",
+    lat: 21.174415,
+    lng: 72.798465,
+    details:
+      "Gujarati School Ghod DoD Road, Athwa, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "Tribhuvan Complex Ghod DoD Road",
+    lat: 21.1748,
+    lng: 72.801528,
+    details:
+      "Tribhuvan Complex Ghod DoD Road, Athwa, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "Rangeela Park Ghod DoD Road",
+    lat: 21.175054,
+    lng: 72.804849,
+    details: "Rangeela Park Ghod DoD Road, Athwa, Surat, Gujarat 395007, India",
+  },
+  {
+    name: "Athwa Pumping Station",
+    lat: 21.177914,
+    lng: 72.804426,
+    details:
+      "Athwa Pumping Station, Arogya Nagar, Athwa, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Mira Nagar Society",
+    lat: 21.174575,
+    lng: 72.811234,
+    details:
+      "Mira Nagar Society, Ram Chowk, Subhash Nagar, Athwa, Surat, Gujarat 395008, India",
+  },
+  {
+    name: "Adarsh Society",
+    lat: 21.177849,
+    lng: 72.811546,
+    details: "Adarsh Society, Athwa, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Adarsh Society",
+    lat: 21.177832,
+    lng: 72.811546,
+    details: "Adarsh Society, Athwa, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "St.Xavier's High School Ghod DoD Road",
+    lat: 21.175717,
+    lng: 72.810182,
+    details:
+      "St.Xavier's High School Ghod DoD Road, Miranagar Society, Athwa, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Panjara Pol Ghod DoD Road",
+    lat: 21.176267,
+    lng: 72.81609,
+    details:
+      "Panjara Pol Ghod DoD Road, IOC Colony, Subhash Nagar, Athwa, Surat, Gujarat 395017, India",
+  },
+  {
+    name: "Income Tax",
+    lat: 21.175848,
+    lng: 72.819244,
+    details: "Income Tax, Khatodra Wadi, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Khatodara Wadi",
+    lat: 21.178197,
+    lng: 72.825199,
+    details: "Khatodara Wadi, Khatodra Wadi, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Galaxy Enclave",
+    lat: 21.190046,
+    lng: 72.774949,
+    details: "Galaxy Enclave, Pal Gam, Surat, Gujarat 394510, India",
+  },
+  {
+    name: "Green City",
+    lat: 21.189712,
+    lng: 72.770735,
+    details: "Green City, Surat, Gujarat 394510, India",
+  },
+  {
+    name: "Green City",
+    lat: 21.18976,
+    lng: 72.770753,
+    details: "Green City, Surat, Gujarat 394510, India",
+  },
+  {
+    name: "Green City Bhatha Approach",
+    lat: 21.18844,
+    lng: 72.767818,
+    details: "Green City Bhatha Approach, Bhatha, Surat, Gujarat 394510, India",
+  },
+  {
+    name: "Green City Bhatha",
+    lat: 21.188631,
+    lng: 72.765808,
+    details: "Green City Bhatha, Bhatha, Surat, Gujarat 394510, India",
+  },
+  {
+    name: "Bhatha Gaam",
+    lat: 21.184328,
+    lng: 72.761888,
+    details: "Bhatha Gaam, Bhatha, Surat, Gujarat 394510, India",
+  },
+  {
+    name: "ONGC Road Junction",
+    lat: 21.190507,
+    lng: 72.722742,
+    details: "ONGC Road Junction, Ichchhapor, Gujarat 394515, India",
+  },
+  {
+    name: "Icchapor Town",
+    lat: 21.191301,
+    lng: 72.72805,
+    details: "Icchapor Town, Ichchhapor, Gujarat 394510, India",
+  },
+  {
+    name: "Hotel Excellency Icchapor",
+    lat: 21.19156,
+    lng: 72.731091,
+    details: "Hotel Excellency Icchapor, Ichchhapor, Gujarat 394510, India",
+  },
+  {
+    name: "Rupali Junction BRTS",
+    lat: 21.170972,
+    lng: 72.816412,
+    details:
+      "Rupali Junction BRTS, Janta Nagar, Navrachna Society, Bhatar, Athwa, Surat, Gujarat 395017, India",
+  },
+  {
+    name: "Jamna Nagar Junction BRTS",
+    lat: 21.16958,
+    lng: 72.810913,
+    details:
+      "Jamna Nagar Junction BRTS, Jeevkar Nagar, Athwa, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Trimurti Apartment",
+    lat: 21.170113,
+    lng: 72.811664,
+    details:
+      "Trimurti Apartment, Subhash Nagar, Athwa, Surat, Gujarat 395008, India",
+  },
+  {
+    name: "Vivekanand Garden",
+    lat: 21.169161,
+    lng: 72.811787,
+    details:
+      "Vivekanand Garden, Poonam Nagar, Bhatar, Athwa, Surat, Gujarat 395017, India",
+  },
+  {
+    name: "Fruit Market",
+    lat: 21.167021,
+    lng: 72.814058,
+    details: "Fruit Market, Bhatar, Athwa, Surat, Gujarat 395017, India",
+  },
+  {
+    name: "Uma Bhavan Bhatar Road",
+    lat: 21.167728,
+    lng: 72.814428,
+    details:
+      "Uma Bhavan Bhatar Road, Bhatar, Athwa, Surat, Gujarat 395017, India",
+  },
+  {
+    name: "Rupali Canal Junction Bhatar Road",
+    lat: 21.16984,
+    lng: 72.815427,
+    details:
+      "Rupali Canal Junction Bhatar Road, Bhatar, Athwa, Surat, Gujarat 395017, India",
+  },
+  {
+    name: "Sosyo Circle Udhana Magdalla Road",
+    lat: 21.171977,
+    lng: 72.826363,
+    details:
+      "Sosyo Circle Udhana Magdalla Road, Gandhi Kutir, Surat, Gujarat 395017, India",
+  },
+  {
+    name: "Sosyo Circle",
+    lat: 21.171036,
+    lng: 72.826953,
+    details: "Sosyo Circle, Laxmi Nagar, Udhana, Surat, Gujarat 395017, India",
+  },
+  {
+    name: "Khatodara Water Works",
+    lat: 21.174258,
+    lng: 72.829131,
+    details:
+      "Khatodara Water Works, Laxmi Nagar, Udhana, Surat, Gujarat 395017, India",
+  },
+  {
+    name: "Khatodara GIDC",
+    lat: 21.175308,
+    lng: 72.826492,
+    details: "Khatodara GIDC, Khatodra Wadi, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Udhna Darvaja",
+    lat: 21.183411,
+    lng: 72.830622,
+    details:
+      "Udhna Darvaja, Maan Darwaja, Khatodra Wadi, Surat, Gujarat 395008, India",
+  },
+  {
+    name: "Maan Darwaja",
+    lat: 21.183651,
+    lng: 72.833025,
+    details:
+      "Maan Darwaja, Maan Darwaja, Aman Nagar, Surat, Gujarat 395008, India",
+  },
+  {
+    name: "Kinnary Cinema",
+    lat: 21.185512,
+    lng: 72.836802,
+    details:
+      "Kinnary Cinema, Maan Darwaja, Aanjada Nagar, Aman Nagar, Surat, Gujarat 395008, India",
+  },
+  {
+    name: "Janta Hospital",
+    lat: 21.186753,
+    lng: 72.827919,
+    details: "Janta Hospital, Sagrampura, Surat, Gujarat 395008, India",
+  },
+  {
+    name: "S.M.C. Water Department",
+    lat: 21.188813,
+    lng: 72.824399,
+    details: "S.M.C. Water Department, Rudrapura, Surat, Gujarat 395001, India",
+  },
+  {
+    name: "Navsari Bazar",
+    lat: 21.189013,
+    lng: 72.826653,
+    details: "Navsari Bazar, Rustampura, Surat, Gujarat 395008, India",
+  },
+  {
+    name: "Gopi Talav Market",
+    lat: 21.190094,
+    lng: 72.827747,
+    details: "Gopi Talav Market, Rustampura, Surat, Gujarat 395008, India",
+  },
+  {
+    name: "Halpati Colony",
+    lat: 21.183912,
+    lng: 72.838991,
+    details: "Halpati Colony, Nehru Nagar, Surat, Gujarat 395008, India",
+  },
+  {
+    name: "5RQR+62X",
+    lat: 21.188113,
+    lng: 72.840106,
+    details:
+      "5RQR+62X, Kamela Rd, Sahara Darwaja, New Textile Market, Surat, Gujarat 395002, India",
+  },
+  {
+    name: "1120/21Vankar",
+    lat: 21.190974,
+    lng: 72.842295,
+    details:
+      "1120/21Vankar, J.J. Market, Moti Begumwadi, New Textile Market, Surat, Gujarat 395002, India",
+  },
+  {
+    name: "Sahara Darwaja",
+    lat: 21.192175,
+    lng: 72.842875,
+    details:
+      "Sahara Darwaja, J.J. Market, Moti Begumwadi, New Textile Market, Surat, Gujarat 395101, India",
+  },
+  {
+    name: "Sahara Darwaja",
+    lat: 21.194515,
+    lng: 72.84457,
+    details:
+      "Sahara Darwaja, Sahara Darwaja, New Textile Market, Surat, Gujarat 395101, India",
+  },
+  {
+    name: "Sahara Darwaja",
+    lat: 21.194715,
+    lng: 72.84354,
+    details:
+      "Sahara Darwaja, Sahara Darwaja, New Textile Market, Surat, Gujarat 395101, India",
+  },
+  {
+    name: "Sai Darshan Market",
+    lat: 21.187433,
+    lng: 72.843068,
+    details:
+      "Sai Darshan Market, Sahara Darwaja, Moti Begumwadi, New Textile Market, Surat, Gujarat 395002, India",
+  },
+  {
+    name: "Linear Bus Stop",
+    lat: 21.202463,
+    lng: 72.839463,
+    details:
+      "Linear Bus Stop, Ring Rd, Railway Station Area, Varachha, Surat, Gujarat 395003, India",
+  },
+  {
+    name: "Delhi Gate Surat",
+    lat: 21.203357,
+    lng: 72.83995,
+    details:
+      "Delhi Gate Surat, Railway Station Area, Varachha, Surat, Gujarat 395008, India",
+  },
+  {
+    name: "Lambe Hanuman Temple",
+    lat: 21.204349,
+    lng: 72.842798,
+    details:
+      "Lambe Hanuman Temple, Saify Society, Varachha, Surat, Gujarat 395008, India",
+  },
+  {
+    name: "Tekrawala School",
+    lat: 21.209249,
+    lng: 72.793275,
+    details:
+      "Tekrawala School, Pushpak Society, Adajan, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "bsnl Office",
+    lat: 21.208144,
+    lng: 72.789279,
+    details:
+      "bsnl Office, Kedarnath Society, Mahadev Nagar, Pankaj Nagar, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Vichar Kranti Circle",
+    lat: 21.21122,
+    lng: 72.790378,
+    details:
+      "Vichar Kranti Circle, New Mahalaxmi Society, Adajan, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Bhagyodaya Society",
+    lat: 21.211105,
+    lng: 72.788356,
+    details:
+      "Bhagyodaya Society, Dindayal Society, Palanpur Patia, Surat, Gujarat 395005, India",
+  },
+  {
+    name: "Palanpur Patiya Circle",
+    lat: 21.21147,
+    lng: 72.790083,
+    details:
+      "Palanpur Patiya Circle, Dindayal Society, Palanpur Patia, Surat, Gujarat 395005, India",
+  },
+  {
+    name: "Housing Board Palanpur",
+    lat: 21.21307,
+    lng: 72.787868,
+    details:
+      "Housing Board Palanpur, Dindayal Society, Palanpur Patia, Surat, Gujarat 395005, India",
+  },
+  {
+    name: "Ramnagar",
+    lat: 21.21368,
+    lng: 72.79199,
+    details:
+      "Ramnagar, Rang Avadhut Society-1, Ramnagar, Sima Nagar, Surat, Gujarat 395005, India",
+  },
+  {
+    name: "Ramnagar BRTS",
+    lat: 21.2162,
+    lng: 72.791422,
+    details:
+      "Ramnagar BRTS, NH228, Ramnagar, Sima Nagar, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "Ram Nagar",
+    lat: 21.217361,
+    lng: 72.787321,
+    details: "Ram Nagar, Ramnagar, Sima Nagar, Surat, Gujarat 395005, India",
+  },
+  {
+    name: "Anand Nagar Society",
+    lat: 21.219961,
+    lng: 72.787213,
+    details:
+      "Anand Nagar Society, Anand Nagar Society, Morabhagal, Surat, Gujarat 395005, India",
+  },
+  {
+    name: "Gayatri Mandir",
+    lat: 21.222401,
+    lng: 72.787278,
+    details:
+      "Gayatri Mandir, Dattatrey Society, Morabhagal, Surat, Gujarat 395005, India",
+  },
+  {
+    name: "Devashish Nagar Mora Bhagal",
+    lat: 21.221881,
+    lng: 72.785218,
+    details:
+      "Devashish Nagar Mora Bhagal, Devashish Nagar, Morabhagal, Surat, Gujarat 395005, India",
+  },
+  {
+    name: "Shreenathaji Haveli Ramnagar",
+    lat: 21.219121,
+    lng: 72.785089,
+    details:
+      "Shreenathaji Haveli Ramnagar, Gopinath Society, Ramnagar, Sima Nagar, Surat, Gujarat 395005, India",
+  },
+  {
+    name: "Raj Harmony",
+    lat: 21.221461,
+    lng: 72.777965,
+    details: "Raj Harmony, Surat, Gujarat 395005, India",
+  },
+  {
+    name: "Anand Villa",
+    lat: 21.220881,
+    lng: 72.774146,
+    details:
+      "Anand Villa, Prabhudarshan Society, Dahin Nagar, Surat, Gujarat 395005, India",
+  },
+  {
+    name: "SMC Staff Quarters",
+    lat: 21.218381,
+    lng: 72.777879,
+    details:
+      "SMC Staff Quarters, Jahangirabad, Sima Nagar, Surat, Gujarat 395005, India",
+  },
+  {
+    name: "Tapash Nagar",
+    lat: 21.21514,
+    lng: 72.777514,
+    details:
+      "Tapash Nagar, Prashant Nagar, Jahangirabad, Sima Nagar, Surat, Gujarat 395009, India",
+  },
+  {
+    name: "SMC Ward Office",
+    lat: 21.211865,
+    lng: 72.77715,
+    details:
+      "SMC Ward Office, Prashant Nagar, Jahangirabad, Palanpur Jakatnaka, Surat, Gujarat 395009, India",
+  },
+];
